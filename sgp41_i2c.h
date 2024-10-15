7dad94036be946313e06eb29d8a379d48c15ecf0
Soiz5644 <soizic.gibeaux@univ-ubs.fr>
2024-10-15 14:43:11 +0200

    Update sgp41_i2c.h

--- sgp41_i2c.h
+++ sgp41_i2c.h
@@ -11,6 +11,8 @@ int16_t sgp41_measure_raw_signal(uint16_t relative_humidity, uint16_t temperatur
 int16_t sgp41_execute_self_test(uint16_t* test_result);
 int16_t sgp41_turn_heater_off(void);
 int16_t sgp41_get_serial_number(uint16_t* serial_number, uint8_t serial_number_size);
+int16_t sgp41_execute_conditioning(uint16_t default_rh, uint16_t default_t, uint16_t *sraw_voc);
+int16_t sgp41_measure_raw_signals(uint16_t default_rh, uint16_t default_t, uint16_t *sraw_voc, uint16_t *sraw_nox);
 
 #ifdef __cplusplus
 }
