5df38c39de164432bffd79894491f46058e5f09b
Soiz5644 <soizic.gibeaux@univ-ubs.fr>
2024-10-29 09:32:03 +0100

    commit

--- Combined/SHT35_SGP30/main_baseline_min.cpp
+++ Combined/SHT35_SGP30/main_baseline_min.cpp
@@ -1,3 +1,60 @@
+#include "rpi_tca9548a.h"
+#include "sgp30_i2c.h"
+#include "sensirion_i2c_hal.h"
+#include "sht3x_i2c.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <fstream>
+#include <cstdlib> // for system()
+#include <ctime>   // for std::time_t, std::tm, std::localtime, std::strftime
+
+#define BASELINE_FILE "sgp30_baseline.txt"
+#define DEBUG_FILE "debug_log.txt"
+#define HOURS_TO_RUN 16
+#define NO_ERROR 0 // Ensure NO_ERROR is defined
+
+// Function to log debug information
+void log_debug_info(const std::string& message) {
+    std::ofstream debug_file(DEBUG_FILE, std::ios::app);
+    if (debug_file.is_open()) {
+        // Get current time
+        std::time_t now = std::time(nullptr);
+        std::tm* local_time = std::localtime(&now);
+        char time_str[100];
+        std::strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", local_time);
+
+        debug_file << "[" << time_str << "] " << message << std::endl;
+        debug_file.close();
+    } else {
+        std::cerr << "Unable to open debug log file" << std::endl;
+    }
+}
+
+// Function to read baseline from file
+bool read_baseline(uint16_t &co2, uint16_t &tvoc) {
+    std::ifstream infile(BASELINE_FILE);
+    if (infile.good()) {
+        infile >> co2 >> tvoc;
+        infile.close();
+        return true;
+    }
+    return false;
+}
+
+// Function to write baseline to file
+void write_baseline(uint16_t co2, uint16_t tvoc) {
+    log_debug_info("Attempting to write baseline to file.");
+    std::ofstream outfile(BASELINE_FILE);
+    if (outfile.is_open()) {
+        outfile << co2 << " " << tvoc;
+        outfile.close();
+        log_debug_info("Baseline written to file: CO2 = " + std::to_string(co2) + ", TVOC = " + std::to_string(tvoc));
+    } else {
+        log_debug_info("Failed to open baseline file for writing.");
+    }
+}
+
 int main() {
     log_debug_info("Program started.");
     sensirion_i2c_hal_init();
