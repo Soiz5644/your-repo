27caf2f4f7d5f5842a33e9dab3ec443bad5b574a
Soiz5644 <soizic.gibeaux@univ-ubs.fr>
2024-11-04 20:15:49 +0100

    commit

--- HM3301/HM3301_TCA9548A/test_HM3301.cpp
+++ HM3301/HM3301_TCA9548A/test_HM3301.cpp
@@ -2,14 +2,24 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 #include "Seeed_HM330X.h"
+#include "rpi_tca9548a.h"
 
 #define HM3301_ADDR 0x40
+#define TCA9548A_ADDR 0x70
+#define TCA9548A_CHANNEL 5  // Channel 5
 
 int main() {
+    // Initialize the TCA9548A
+    rpi_tca9548a tca9548a(TCA9548A_ADDR);
+    if (!tca9548a.selectChannel(TCA9548A_CHANNEL)) {
+        std::cerr << "Failed to select channel on TCA9548A" << std::endl;
+        return 1;
+    }
+
+    // Initialize the HM3301 sensor
     HM330X sensor(HM3301_ADDR);
     uint8_t data[29];
 
-    // Initialize sensor
     if (sensor.init() == NO_ERROR) {
         if (sensor.read_sensor_value(data, 29) == NO_ERROR) {
             std::cout << "Sensor Data: ";
