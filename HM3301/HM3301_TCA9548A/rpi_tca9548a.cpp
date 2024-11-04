ed274193e62f365ae8adb31476ba9270affedd58
Soiz5644 <soizic.gibeaux@univ-ubs.fr>
2024-11-04 19:09:51 +0100

    commit

--- HM3301/HM3301_TCA9548A/rpi_tca9548a.cpp
+++ HM3301/HM3301_TCA9548A/rpi_tca9548a.cpp
@@ -6,6 +6,7 @@ Author: Bryan Casanelli - bryancasanelli@gmail.com
 #include "rpi_tca9548a.h"
 #include <wiringPiI2C.h>
 #include <wiringPi.h>
+#include <iostream>
 
 rpi_tca9548a::rpi_tca9548a(){
   wiringPiSetup();
@@ -17,14 +18,22 @@ rpi_tca9548a::~rpi_tca9548a(){
 
 int rpi_tca9548a::init(int id){
   this->fd = wiringPiI2CSetup(id);
-  if (this->fd == -1){return -1;} // Error
-  return 0;                       // Ok
+  if (this->fd == -1){
+    std::cerr << "Failed to initialize TCA9548A at address: " << id << std::endl;
+    return -1; // Error
+  }
+  return 0; // Ok
 }
 
 void rpi_tca9548a::set_channel(uint8_t channel){
-  wiringPiI2CWrite(this->fd,1<<channel);
+  int ret = wiringPiI2CWrite(this->fd, 1 << channel);
+  if (ret != 0) {
+    std::cerr << "Failed to select channel: " << (int)channel << std::endl;
+  } else {
+    std::cout << "Selected channel: " << (int)channel << std::endl;
+  }
 }
 
 void rpi_tca9548a::no_channel(){
-  wiringPiI2CWrite(this->fd,0);
+  wiringPiI2CWrite(this->fd, 0);
 }
