e86e5311246dcd46a8397ae65067203ee90abd03
Soiz5644 <soizic.gibeaux@univ-ubs.fr>
2024-11-20 16:37:26 +0100

    commit

---
+++ SGP30/SGP30_TCA9548A/rpi_tca9548apigpio.h
@@ -0,0 +1,18 @@
+#ifndef RPI_TCA9548A_H
+#define RPI_TCA9548A_H
+
+#include <stdint.h>
+
+class rpi_tca9548a {
+public:
+    rpi_tca9548a();
+    ~rpi_tca9548a();
+    int init(int id);
+    int set_channel(uint8_t channel);
+    void no_channel();
+  
+private:
+    int i2c_handle; // This replaces the previous 'fd'
+};
+
+#endif
