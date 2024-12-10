d9dd6628095b89f916a5483904b2cb9745e2c345
Soiz5644 <soizic.gibeaux@univ-ubs.fr>
2024-12-10 11:11:45 +0100

    commit

--- Combined/FS3000_SGP30_SGP40_SGP41_SHT35/sgp30_i2c.h
+++ Combined/FS3000_SGP30_SGP40_SGP41_SHT35/sgp30_i2c.h
@@ -1,20 +1,24 @@
 #ifndef SGP30_I2C_H
 #define SGP30_I2C_H
 
-#include <stdint.h>
-
 class SGP30 {
 public:
     SGP30();
+
     int init();
     int measure();
     int readRaw();
+
     uint16_t getTVOC();
     uint16_t getCO2();
     uint16_t getH2_raw();
     uint16_t getEthanol_raw();
+
     float getH2();
     float getEthanol();
+
+    int set_absolute_humidity(float absoluteHumidity);
+    int set_relative_humidity(float T, float RH);
 
 private:
     uint16_t co2_eq_ppm;
